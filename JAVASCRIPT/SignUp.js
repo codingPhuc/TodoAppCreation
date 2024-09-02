4535a1078181f9c9023253ca5482aaf1a41032fe
Nguyen Hoang Phuc <phucnguyenhoang3839@gmail.com>
2024-09-02 16:19:26 +0700

    Phuc | change to signup

--- JAVASCRIPT/SignUp.js
+++ JAVASCRIPT/SignUp.js
@@ -41,3 +41,29 @@ document
   .getElementById("sign-in-redirect-button")
   .addEventListener("click", redirectLogin);
 document.getElementById("sign-up-button").addEventListener("click", signup);
+
+function toWeirdCase(string) {
+  var words = string.split(" ");
+  for (var i = 0; i < words.length; i++) {
+    var characters = words[i].split("");
+    for (var j = 0; j < characters.length; j++) {
+      if (j % 2 === 0) {
+        characters[j] = characters[j].toUpperCase();
+      } else {
+        characters[j] = characters[j].toLowerCase();
+      }
+    }
+    words[i] = characters.join("");
+  }
+  var result = words.join(" ");
+  return result;
+}
+
+var countBits = function (n) {
+  let countNumberOfBits = 0;
+  while (n > 0) {
+    if (n % 2 == 1) countNumberOfBits++;
+    n = Math.floor(n / 2);
+  }
+  return countNumberOfBits;
+};
